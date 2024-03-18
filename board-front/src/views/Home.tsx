4e026b270fb579a2d00387eff79fcdf5981b7e29
Lee <jh990714@naver.com>
2024-03-18 20:07:38 +0900

    충돌

--- board-front/src/views/Home.tsx
+++ board-front/src/views/Home.tsx
@@ -10,7 +10,6 @@ export const Home: React.FC = () => {
       <div className='carouselContainer'>
         <CarouselComp img={img} />
       </div>
-      <div>dd</div>
       <BestProduct category='./bestProducts'/>
       <BestProduct category='./newProducts'/>
       <BestProduct category='./allProducts'/>
