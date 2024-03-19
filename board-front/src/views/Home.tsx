413ff981f50d0e09dbff1a9f6c83239177bfe27e
장준혁 <jh990714@naver.com>
2024-03-19 14:55:40 +0900

    IntersectionObserver 적용/ 애니메이션 효과

--- board-front/src/views/Home.tsx
+++ board-front/src/views/Home.tsx
@@ -4,24 +4,35 @@ import CarouselComp from 'components/CarouselComp';
 import img from '../assets/testimg.jpg'
 import { BestProduct } from 'components/BestProduct';
 import { HomeBanner1 } from 'components/HomeBanner/HomeBanner1';
-import { HomeBanner2 } from 'components/HomeBanner/HomeBanner2';
-
+import { useAnimateOnScroll } from 'hook/useAnimateOnScroll '
 import './Home.css'
 
 export const Home: React.FC = () => {
+  const ref1 = useAnimateOnScroll();
+  const ref2 = useAnimateOnScroll();
+  const ref3 = useAnimateOnScroll();
+  
   return (
     <div className='homeContainer'>
       <div className='carouselContainer'>
         <CarouselComp img={img} />
       </div>
+
       <div className='HomeBanner1'>
         <HomeBanner1 />
       </div>
-      <div>ss</div>
-      
-      <BestProduct category='./bestProducts'/>
-      <BestProduct category='./newProducts'/>
-      <BestProduct category='./allProducts'/>
+
+      <div ref={ref1}>
+        <BestProduct category='./bestProducts'/>
+      </div>
+
+      <div ref={ref2}>
+        <BestProduct category='./newProducts'/>
+      </div>
+
+      <div ref={ref3}>
+        <BestProduct category='./allProducts'/>
+      </div>
     </div>
   );
 };
