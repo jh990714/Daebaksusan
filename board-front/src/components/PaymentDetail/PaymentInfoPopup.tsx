58bed653289066ce5af23fecf50bb3e3db0dfd8c
장준혁 <jh990714@naver.com>
2024-04-21 22:45:42 +0900

    whitespace-wrap 추가

--- board-front/src/components/PaymentDetail/PaymentInfoPopup.tsx
+++ board-front/src/components/PaymentDetail/PaymentInfoPopup.tsx
@@ -87,7 +87,7 @@ export const PaymentInfoPopup: React.FC<PaymentInfoPopupProps> = ({ onClose, ord
         <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50 text-sm">
             <div className="bg-white p-8 rounded-lg max-w-xl w-full overflow-y-auto max-h-[60%] ">
                 <div className="flex justify-between items-center mb-6">
-                    <h2 className="m-0 text-lg font-bold">주문 정보</h2>
+                    <h2 className="m-0 text-lg font-bold ">주문 정보</h2>
                     <button className="text-gray-500 hover:text-gray-700" onClick={onClose}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
@@ -97,28 +97,28 @@ export const PaymentInfoPopup: React.FC<PaymentInfoPopupProps> = ({ onClose, ord
                 <table className="w-full mb-6 text-left">
                     <tbody className="divide-y divide-gray-200">
                         <tr>
-                            <td className="font-bold pr-4 py-2">주문 번호:</td>
+                            <td className="font-bold pr-4 py-2 whitespace-nowrap">주문 번호:</td>
                             <td className="py-2">{paymentInfo?.orderNumber}</td>
                         </tr>
                         <tr>
-                            <td className="font-bold pr-4 py-2">주문 일시:</td>
+                            <td className="font-bold pr-4 py-2 whitespace-nowrap">주문 일시:</td>
                             <td className="py-2">{paymentInfo && paymentInfo.orderAt ? new Date(paymentInfo.orderAt * 1000).toLocaleString() : '-'}</td>
 
                         </tr>
                         <tr>
-                            <td className="font-bold pr-4 py-2">상품명:</td>
+                            <td className="font-bold pr-4 py-2 whitespace-nowrap">상품명:</td>
                             <td className="py-2">{paymentInfo?.productName}</td>
                         </tr>
                         <tr>
-                            <td className="font-bold pr-4 py-2">수령인:</td>
+                            <td className="font-bold pr-4 py-2 whitespace-nowrap">수령인:</td>
                             <td className="py-2">{paymentInfo?.name}</td>
                         </tr>
                         <tr>
-                            <td className="font-bold pr-4 py-2">배송 주소:</td>
+                            <td className="font-bold pr-4 py-2 whitespace-nowrap">배송 주소:</td>
                             <td className="py-2">{paymentInfo?.address}</td>
                         </tr>
                         <tr>
-                            <td className="font-bold pr-4 py-2">연락처:</td>
+                            <td className="font-bold pr-4 py-2 whitespace-nowrap">연락처:</td>
                             <td className="py-2">{paymentInfo?.phone}</td>
                         </tr>
                     </tbody>
@@ -131,13 +131,13 @@ export const PaymentInfoPopup: React.FC<PaymentInfoPopupProps> = ({ onClose, ord
                 <table className="w-full mb-6 text-start">
                     <tbody className="divide-y divide-gray-200">
                         <tr>
-                            <td className="font-bold pr-4 py-2">결제 일시:</td>
+                            <td className="font-bold pr-4 py-2 whitespace-nowrap">결제 일시:</td>
                             <td className="py-2">{paymentInfo && paymentInfo.paymentAt ? new Date(paymentInfo.paymentAt).toLocaleString() : '-'}</td>
 
                         </tr>
 
                         <tr>
-                            <td className="font-bold pr-4 py-2">결제 수단:</td>
+                            <td className="font-bold pr-4 py-2 whitespace-nowrap">결제 수단:</td>
                             <td className="py-2">{paymentInfo?.paymentMethod}</td>
                         </tr>
 
@@ -147,19 +147,19 @@ export const PaymentInfoPopup: React.FC<PaymentInfoPopupProps> = ({ onClose, ord
                         {paymentInfo?.paymentMethod === '무통장 입금' && (
                             <>
                                 <tr>
-                                    <td className="font-bold pr-4 py-2">입금 은행:</td>
+                                    <td className="font-bold pr-4 py-2 whitespace-nowrap">입금 은행:</td>
                                     <td className="py-2">{paymentInfo?.vbankName}</td>
                                 </tr>
 
                                 <tr>
-                                    <td className="font-bold pr-4 py-2">가상 계좌 번호:</td>
+                                    <td className="font-bold pr-4 py-2 whitespace-nowrap">가상 계좌 번호:</td>
                                     <td className="py-2">{paymentInfo?.vbankNum}</td>
                                 </tr>
                             </>
                         )}
                         {paymentInfo?.paymentMethod === '계좌이체' && (
                             <tr>
-                                <td className="font-bold pr-4 py-2">입금 은행:</td>
+                                <td className="font-bold pr-4 py-2 whitespace-nowrap">입금 은행:</td>
                                 <td className="py-2">{paymentInfo?.bankName}</td>
                             </tr>
 
@@ -168,27 +168,27 @@ export const PaymentInfoPopup: React.FC<PaymentInfoPopupProps> = ({ onClose, ord
                         {paymentInfo?.paymentMethod === '신용/체크카드' && (
                             <>
                                 <tr>
-                                    <td className="font-bold pr-4 py-2">카드 종류:</td>
+                                    <td className="font-bold pr-4 py-2 whitespace-nowrap">카드 종류:</td>
                                     <td className="py-2">{paymentInfo?.cardName}</td>
                                 </tr>
                                 <tr>
-                                    <td className="font-bold pr-4 py-2">할부 개월 수:</td>
+                                    <td className="font-bold pr-4 py-2 whitespace-nowrap">할부 개월 수:</td>
                                     <td className="py-2">{paymentInfo?.installmentMonths ? `${paymentInfo.installmentMonths}개월` : ''}</td>
                                 </tr>
                                 <tr>
-                                    <td className="font-bold pr-4 py-2">카드 번호:</td>
+                                    <td className="font-bold pr-4 py-2 whitespace-nowrap">카드 번호:</td>
                                     <td className="py-2">{paymentInfo?.cardNumber}</td>
                                 </tr>
                             </>
                         )}
 
                         <tr>
-                            <td className="font-bold pr-4 py-2">결제 금액:</td>
+                            <td className="font-bold pr-4 py-2 whitespace-nowrap">결제 금액:</td>
                             <td className="py-2">{paymentInfo?.amount.toLocaleString()} 원</td>
                         </tr>
 
                         <tr>
-                            <td className="font-bold pr-4 py-2">결제 상태:</td>
+                            <td className="font-bold pr-4 py-2 whitespace-nowrap">결제 상태:</td>
                             <td className="py-2">{paymentInfo?.paymentStatus}</td>
                         </tr>
                     </tbody>
@@ -198,7 +198,7 @@ export const PaymentInfoPopup: React.FC<PaymentInfoPopupProps> = ({ onClose, ord
                     <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50">
                         <div className="bg-white p-8 rounded-lg max-w-md w-full">
                             <p>결제를 취소하시겠습니까?</p>
-                            <div className="flex justify-center mt-4">
+                            <div className="flex justify-center mt-4 whitespace-nowrap">
                                 <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-4" onClick={confirmCancel}>확인</button>
                                 <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded" onClick={() => setShowCancelConfirmation(false)}>취소</button>
                             </div>
@@ -207,7 +207,7 @@ export const PaymentInfoPopup: React.FC<PaymentInfoPopupProps> = ({ onClose, ord
                 )}
 
                 {paymentInfo?.paymentStatus !== '결제 취소' && (
-                    <div className="flex justify-center">
+                    <div className="flex justify-center whitespace-nowrap">
                         <button
                             className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4"
                             onClick={handleCancel}
