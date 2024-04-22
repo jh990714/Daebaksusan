e6129b58deaffd7cda9bb5c7d3e87df27d57cdb3
장준혁 <jh990714@naver.com>
2024-04-22 23:56:05 +0900

    쿠폰 이미지 생성

---
+++ board-front/src/views/MyCoupons.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { Coupon } from 'types';
+import { CouponComp } from 'components/Coupon/CouponComp';
+import { Pagination } from 'components/Pagination';
+
+interface MyCouponsProps {
+  coupons?: Coupon[];
+}
+
+export const MyCoupons: React.FC<MyCouponsProps> = ({ coupons }) => {
+  const pageSize = 5; // 페이지당 쿠폰 수
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // 현재 페이지에 따른 쿠폰 목록 계산
+  const currentCoupons = coupons?.slice((currentPage - 1) * pageSize, currentPage * pageSize);
+
+  // 페이지 변경 함수
+  const handlePageChange = (page: number) => setCurrentPage(page);
+
+  return (
+    <div>
+      {currentCoupons?.map((coupon, index) => (
+        <div key={index} className="bg-white border rounded-lg shadow-md p-1 m-3">
+          <CouponComp coupon={coupon} />
+        </div>
+      ))}
+      {coupons && (
+        <Pagination
+          pageSize={pageSize}
+          totalPages={Math.ceil(coupons.length / pageSize)}
+          currentPage={currentPage}
+          onPageChange={handlePageChange}
+        />
+      )}
+    </div>
+  );
+};
