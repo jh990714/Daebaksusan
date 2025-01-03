c6664438188b498276e95ea517e767721eb974d3
장준혁 <jh990714@naver.com>
2025-01-04 00:44:49 +0900

    댓글목록 아이콘으로 변경

--- board-front/src/components/DetailTab/QnAItemComp.tsx
+++ board-front/src/components/DetailTab/QnAItemComp.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { QnA } from 'types';
 import styles from './QnAItemComp.module.css';
+import talkIcon from '../../assets/talkIcon.png';
 
 interface QnAItemCompProps {
     qnaList: QnA[];
@@ -25,7 +26,7 @@ export const QnAItemComp: React.FC<QnAItemCompProps> = ({ qnaList }) => {
                         <div className={styles.question}>
                             <div className={styles.userInfo}>
                                 <span><span className={styles.questionLable}>질문</span> {qna.question.name}</span>
-                                <span>{new Date(qna.question.createdAt).toLocaleString('ko-KR')}</span>
+                                <span className={styles.date}>{new Date(qna.question.createdAt).toLocaleString('ko-KR')}</span>
                             </div>
 
                             <div className={styles.questionContent}>
@@ -40,13 +41,28 @@ export const QnAItemComp: React.FC<QnAItemCompProps> = ({ qnaList }) => {
                     }
 
                     {/* 답변 보기/숨기기 버튼 (답변이 있을 경우에만 표시) */}
-                    {qna.answers.length > 0 && (
+                    {/* {qna.answers.length > 0 && (
                         <div className="flex justify-center">
                             <button
                                 onClick={() => toggleAnswers(index)}
                                 className="w-full sm:w-auto px-4 py-2 mb-3 bg-blue-600 text-white font-semibold rounded-md shadow-md hover:bg-blue-700 transition duration-300 ease-in-out"
                             >
                                 {showAnswers[index] ? `답변 숨기기 (${qna.answers.length})` : `답변 보기 (${qna.answers.length})`}
+                            </button>
+                        </div>
+                    )} */}
+
+                    {qna.answers.length > 0 && (
+                        <div className="flex justify-end"> {/* Flex 컨테이너로 우측 정렬 */}
+                            <button
+                                onClick={() => toggleAnswers(index)}
+                                className="flex items-center justify-center"
+                            >
+                                <img
+                                    src={talkIcon}
+                                    alt="Talk Icon"
+                                    className="w-10 h-10"
+                                />
                             </button>
                         </div>
                     )}
@@ -58,7 +74,7 @@ export const QnAItemComp: React.FC<QnAItemCompProps> = ({ qnaList }) => {
                             <div className={styles.answer}>
                                 <div className={styles.userInfo}>
                                     <span><span className={styles.answerLable}>답변</span> 대박수산</span>
-                                    <span>{new Date(answer.createdAt).toLocaleString('ko-KR')}</span>
+                                    <span className={styles.date}>{new Date(answer.createdAt).toLocaleString('ko-KR')}</span>
                                 </div>
                                 <div className={styles.answerContent}>
                                     {answer.content.split('\n').map((line, index) => (
